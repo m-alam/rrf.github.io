593c613b45df04811a0f26ef6b1d1392d345dad2
m-alam <m.m.alam6bs@gmail.com>
2017-06-10 00:57:45 +0600

    class31

---
+++ class31/assets/js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        var s =skrollr.init({
+        	render: function(data){
+        	//Log the current scroll position.
+		//console.log(data.curTop);
+	}
+        })
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
