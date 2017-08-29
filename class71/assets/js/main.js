c2c870daffc7fe41fb4c84e6684738a00a743b79
m-alam <m.m.alam6bs@gmail.com>
2017-08-29 09:13:30 +0600

    class71

---
+++ class71/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+
+        
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
